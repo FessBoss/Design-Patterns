334574a1c5e7d90625a46d6b0400a8bf461bfd0e
Evgeny Mereha <skyflex713@gmail.com>
2021-02-10 07:58:05 +0700

    Added first example for pattern Command

---
+++ Command/src/Example1/UpdateCommand.java
@@ -0,0 +1,14 @@
+package Example1;
+
+public class UpdateCommand implements Command {
+    DataBase dataBase;
+
+    public UpdateCommand(DataBase dataBase) {
+        this.dataBase = dataBase;
+    }
+
+    @Override
+    public void execute() {
+        dataBase.update();
+    }
+}
