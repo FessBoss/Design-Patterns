c63565a8138f11879ba657fced75b80c73667d2d
Evgeny Mereha <skyflex713@gmail.com>
2021-02-12 10:54:17 +0700

    Added second example for pattern Adapter

---
+++ Adapter-Facade/src/Adapter/Example2/AdapterJavaToDatabase.java
@@ -0,0 +1,23 @@
+package Adapter.Example2;
+
+public class AdapterJavaToDatabase extends JavaApplication implements DataBase {
+    @Override
+    public void insert() {
+        saveObject();
+    }
+
+    @Override
+    public void update() {
+        updateObject();
+    }
+
+    @Override
+    public void select() {
+        loadObject();
+    }
+
+    @Override
+    public void remove() {
+        deleteObject();
+    }
+}
---
+++ Adapter-Facade/src/Adapter/Example2/DataBase.java
@@ -0,0 +1,8 @@
+package Adapter.Example2;
+
+public interface DataBase {
+    public void insert();
+    public void update();
+    public void select();
+    public void remove();
+}
