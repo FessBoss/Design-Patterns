3aa661c9cacdadb26bcb4dbda3d3f8b0441a66d7
Evgeny Mereha <skyflex713@gmail.com>
2021-02-13 10:24:30 +0700

    Added second example for pattern Facade

---
+++ Adapter-Facade/src/Facade/Example2/Job.java
@@ -0,0 +1,7 @@
+package Facade.Example2;
+
+public class Job {
+    public void doJob() {
+        System.out.println("Job is progress...");
+    }
+}
---
+++ Adapter-Facade/src/Facade/Example2/SprintRunner.java
@@ -0,0 +1,8 @@
+package Facade.Example2;
+
+public class SprintRunner {
+    public static void main(String[] args) {
+        Workflow workflow = new Workflow();
+        workflow.solveProblems();
+    }
+}
