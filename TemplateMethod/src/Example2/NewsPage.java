b6bd6074d811e84586eb341c00efd9e0d861bf5e
Evgeny Mereha <skyflex713@gmail.com>
2021-02-13 13:46:15 +0700

    Added second example for Template Method

---
+++ TemplateMethod/src/Example2/NewsPage.java
@@ -0,0 +1,8 @@
+package Example2;
+
+public class NewsPage extends WebsiteTemplate {
+    @Override
+    public void showPageContent() {
+        System.out.println("News");
+    }
+}
---
+++ TemplateMethod/src/Example2/WelcomePage.java
@@ -0,0 +1,8 @@
+package Example2;
+
+public class WelcomePage extends WebsiteTemplate {
+    @Override
+    public void showPageContent() {
+        System.out.println("Welcome");
+    }
+}
